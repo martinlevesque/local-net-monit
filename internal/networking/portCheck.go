b646eeb38dbb0d098fba7b7f5fd2456d53b2595d
Martin Levesque <levesque.martin@gmail.com>
2024-08-27 21:27:12 -0400

    wip

--- internal/networking/portCheck.go
+++ internal/networking/portCheck.go
@@ -10,11 +10,30 @@ func isUDPPortOpen(host string, port int) bool {
 	address := fmt.Sprintf("%s:%d", host, port)
 	conn, err := net.DialTimeout("udp", address, 2*time.Second)
 
+	defer conn.Close()
+
 	if err != nil {
+		fmt.Println("udp dial error: ", err)
 		return false
 	}
 
-	defer conn.Close()
+	_, err = conn.Write([]byte("test"))
+
+	if err != nil {
+		fmt.Println("udp write error: ", err)
+		return false
+	}
+
+	// Set a read deadline for the response
+	conn.SetReadDeadline(time.Now().Add(2 * time.Second))
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+
+	// If we get an error on read, it's likely the port is closed
+	if err != nil {
+		fmt.Println("udp read error: ", err)
+		return false
+	}
 
 	return true
 }
